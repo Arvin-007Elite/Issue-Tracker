c546dbf6f4b999191eadb8530ce522330e7f3186
Arvin <arvinkritik2@gmail.com>
2023-11-19 05:08:21 +0530

    built new issue page

---
+++ issue-tracker/app/issues/new/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react'
+import { Button, TextField } from '@radix-ui/themes'
+import { TextArea}  from '@radix-ui/themes'
+
+const NewIssuePage = () => {
+  return (
+      <div className='max-w-xl  space-y-3'>
+          <TextField.Root>
+              <TextField.Input placeholder='Title' />
+              
+
+
+          </TextField.Root>
+
+          <TextArea placeholder='Description' />
+          <Button> Submit New Issue </Button>
+    </div>
+  )
+}
+
+export default NewIssuePage
--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Button } from '@radix-ui/themes';
+import { Button, Link } from '@radix-ui/themes';
 
 const IssuesPage = () => {
   return (
-    <div><Button> New Issue</Button></div>
+    <div><Button> <Link href='/issues/new'>New Issue</Link></Button></div>
   )
 }
 
